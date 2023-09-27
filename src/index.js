ffca7aa9c40c982e3deb4c062e42e7262b7c6fb0
vijita-u <udayvijita3009@gmail.com>
2023-09-27 19:44:41 +0530

    Update directoty structure to solve hosting errors on github pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
-import App from "./src/App";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
